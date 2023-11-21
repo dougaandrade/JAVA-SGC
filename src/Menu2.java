3c08753252e8cb7a4fc251f3534a1a6f6ee6cd83
D Ө U G <116695856+dougaandrade@users.noreply.github.com>
2023-11-21 20:42:55 -0300

    Add files via upload

---
+++ src/Menu2.java
@@ -0,0 +1,9 @@
+public class Menu2 {
+
+    public static void GerarMenu2() {
+        System.out.println("1. Dinheiro");
+        System.out.println("2. PIX");
+        System.out.println("3. Cartao de Credito");
+        System.out.println("4. Cartao de Debito");
+    }
+}
